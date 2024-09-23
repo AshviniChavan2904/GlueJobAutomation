f1c3e66f113daee792b0b7df074f64be5ec5312f
Ashvini Chavan <AChavan@TechStyle.com>
2024-09-23 21:14:08 +0530

    modified test job name

--- Script1/read.py
+++ Script1/read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Parquet file
-df = pd.read_parquet('part-00000-c516386c-0f10-4b8c-8649-c9ad65c138b3-c000.snappy.parquet')
+df = pd.read_parquet('part-00000-206e103f-aeca-4142-9fd5-38e101898824-c000.snappy.parquet')
 
 # Show the contents of the Parquet file
 print(df)
