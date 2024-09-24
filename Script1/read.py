0af1b154ef94606f96894fd76d5412faf04318e1
Ashvini Chavan <AChavan@TechStyle.com>
2024-09-24 15:34:01 +0530

    added new wf logic

--- Script1/read.py
+++ Script1/read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Parquet file
-df = pd.read_parquet('part-00000-206e103f-aeca-4142-9fd5-38e101898824-c000.snappy.parquet')
+df = pd.read_parquet('part-00000-5aa330d4-2984-4535-9e81-ba04c0fe602f-c000.snappy.parquet')
 
 # Show the contents of the Parquet file
 print(df)
