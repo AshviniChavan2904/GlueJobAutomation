669fd33f1e0a5e3e4d0be1d80019053b9430cf2b
Ashvini Chavan <AChavan@TechStyle.com>
2024-09-24 15:54:30 +0530

    push only file to s3 not folder

--- Script1/read.py
+++ Script1/read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Parquet file
-df = pd.read_parquet('part-00000-5aa330d4-2984-4535-9e81-ba04c0fe602f-c000.snappy.parquet')
+df = pd.read_parquet('part-00000-9c1d7da4-5a07-4d86-81d9-d7c4edd1c489-c000.snappy.parquet')
 
 # Show the contents of the Parquet file
 print(df)
