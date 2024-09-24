3dcbbf59f078e4f2faa5e7fc193a26a6afc3471d
Ashvini <42347943+AshviniChavan2904@users.noreply.github.com>
2024-09-24 15:59:18 +0530

    Delete Script1/read.py

--- Script1/read.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the Parquet file
-df = pd.read_parquet('part-00000-9c1d7da4-5a07-4d86-81d9-d7c4edd1c489-c000.snappy.parquet')
-
-# Show the contents of the Parquet file
-print(df)
