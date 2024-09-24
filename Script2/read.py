5eca907286d6cb85af1ab147f1b2e5805137ef81
Ashvini Chavan <AChavan@TechStyle.com>
2024-09-24 14:54:42 +0530

    update glue job with script1

--- Script2/read.py
+++ Script2/read.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Parquet file
-df = pd.read_parquet('part-00000-de7ad737-e7f0-4669-b39b-7a9e0cb50a4e-c000.snappy.parquet')
+df = pd.read_parquet('part-00000-f93bae9f-73a7-4859-953c-2b489cad79ec-c000.snappy.parquet')
 
 # Show the contents of the Parquet file
 print(df)
